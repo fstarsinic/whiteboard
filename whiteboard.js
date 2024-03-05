b21b7e6b97a3b832ad32ca063bb793cbbec2fb5d
fstarsinic <frank.starsinic@gmail.com>
2024-03-05 07:23:06 -0800

    Update whiteboard.js

--- whiteboard.js
+++ whiteboard.js
@@ -2,26 +2,54 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 
-function findYamlFilesAndKeys(directory, result = []) {
+function findYamlFilesAndDetails(directory, result = []) {
     const filesAndDirs = fs.readdirSync(directory, { withFileTypes: true });
 
     filesAndDirs.forEach(dirent => {
         const fullPath = path.join(directory, dirent.name);
         if (dirent.isDirectory()) {
-            findYamlFilesAndKeys(fullPath, result); // Recursively search in directories
+            findYamlFilesAndDetails(fullPath, result); // Recursively search in directories
         } else if (path.extname(dirent.name) === '.yaml') {
             const fileContents = fs.readFileSync(fullPath, 'utf8');
             const yamlContents = yaml.load(fileContents);
-            const topKeys = Object.keys(yamlContents).join(', '); // Get top-level keys and join them
-            result.push(`${fullPath}: ${topKeys}`);
+            const details = extractDetails(yamlContents);
+            result.push(`${fullPath}: ${details}`);
         }
     });
 
     return result;
 }
 
-// Replace 'yourStartDirectory' with the path of your directory
-const startDirectory = 'yourStartDirectory'; 
-const yamlFilesAndKeys = findYamlFilesAndKeys(startDirectory);
+function extractDetails(yamlContents) {
+    let details = [];
 
-console.log(yamlFilesAndKeys);
+    // Top-level keys
+    const topKeys = Object.keys(yamlContents).join(', ');
+    details.push(topKeys);
+
+    // info / tags
+    if (yamlContents.info && yamlContents.info.tags) {
+        const tags = yamlContents.info.tags.join(', ');
+        details.push(`tags: ${tags}`);
+    }
+
+    // http / method and http / method / path
+    if (yamlContents.http) {
+        const methods = Object.keys(yamlContents.http).map(method => {
+            let methodDetails = `method: ${method}`;
+            if (yamlContents.http[method].path) {
+                methodDetails += `, path: ${yamlContents.http[method].path}`;
+            }
+            return methodDetails;
+        }).join('; ');
+        details.push(methods);
+    }
+
+    return details.join('; ');
+}
+
+// Replace 'yourStartDirectory' with the path of your directory
+const startDirectory = 'yourStartDirectory';
+const yamlFilesAndDetails = findYamlFilesAndDetails(startDirectory);
+
+console.log(yamlFilesAndDetails);
