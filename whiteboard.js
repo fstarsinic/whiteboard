f6bf7ee80ad869650e11127030569e61ee2f12ae
fstarsinic <frank.starsinic@gmail.com>
2024-03-07 20:01:13 -0800

    Update whiteboard.js

--- whiteboard.js
+++ whiteboard.js
@@ -1,46 +1,34 @@
-const { Client } = require('@opensearch-project/opensearch');
-const fs = require('fs-extra');
-const glob = require('glob-promise');
-
-// Initialize OpenSearch client
-const client = new Client({
-  node: 'yourOpensearchHost',
-  auth: {
-    username: 'yourUsername',
-    password: 'yourPassword',
-  },
-});
-
-// Function to insert a single document into the OpenSearch database
-async function insertDocument(filePath) {
-  try {
-    const content = await fs.readJson(filePath);
-    const response = await client.index({
-      index: 'nuclei',
-      body: content,
-      refresh: true, // Ensure that documents are indexed immediately for the sake of the example
-    });
-
-    console.log(`Document from '${filePath}' has been indexed. Response ID: ${response.body._id}`);
-  } catch (error) {
-    console.error(`Error inserting document from '${filePath}':`, error);
-  }
-}
-
-// Function to find and insert documents from .yaml.json files
-async function insertDocumentsFromYamlJsonFiles(startPath) {
-  try {
-    // Find all .yaml.json files in the directory and subdirectories
-    const files = await glob(`${startPath}/**/*.yaml.json`);
-
-    for (let file of files) {
-      await insertDocument(file); // Call insertDocument for each file
-    }
-  } catch (error) {
-    console.error('An error occurred while searching for files:', error);
-  }
-}
-
-// Replace 'yourStartDirectory' with the path of your directory
-const startDirectory = 'yourStartDirectory';
-insertDocumentsFromYamlJsonFiles(startDirectory);
+def asnDataLoad(ips):
+    for ip in ips:
+        for i in asn_data['services']:
+            ip = int(ip)
+            for iprange in i[0]:
+                x = iprange.split("-")
+                try:
+         
+                    if ip in range(int(x[0]),int(x[1])+1):
+                    #if ip >= int(x[0]) and ip < int(x[1])+1:
+                        print("now in function for ip  in"+str(ip))
+                        data_url = i[1][0]+'autnum/'+str(ip)
+                        #print(data_url)
+                        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'}
+                        #response = requests.get(data_url)  
+                        time.sleep(1)
+                        req = Request(data_url, headers= headers)           
+                        response = urlopen(req)
+                        data_json = json.loads(response.read())          
+                        #return data_json
+                        with open('./raw_asn/'+str(ip)+'.json', 'w') as outfile:
+                            json.dump(data_json, outfile)
+            
+                except:
+                    global  k 
+                    #print(k)
+                    k = k+1
+                    print("failed in"+str(ip))
+                    if k < 3:
+                        print("Retrying again for"+str(ip))
+                        #asnDataLoad(ip)
+            
+                    pass
+    
