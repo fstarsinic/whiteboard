09fe21eb4c11310f4bbfb8363591754917cfdac7
fstarsinic <frank.starsinic@gmail.com>
2024-03-08 12:35:30 -0800

    Update whiteboard.js

--- whiteboard.js
+++ whiteboard.js
@@ -1,5 +1,8 @@
-def writeJson(filenames):
-    #print(filenames)
+import json
+import os
+import time
+
+def writeJson(filenames, path_of_the_directory):
     for filename in filenames:
         print(filename)
         fn = f'{path_of_the_directory}{os.sep}{filename}'
@@ -8,128 +11,65 @@ def writeJson(filenames):
             if not os.path.exists(fn):
                 print(f'{fn} file does not exist')
                 continue
-            with open(fn,'r') as f:
+            with open(fn, 'r') as f:
                 print(filename)
                 time.sleep(1)
                 data = json.loads(f.read())
-                if data != None:
-                    d = {}
-                    d["AS"] = data["handle"]
-                    df_nested_list = pd.json_normalize(data, record_path =["entities"])
+                if data is not None:
+                    d = {"AS": data["handle"]}
+                    entities = data.get("entities", [])
                     jsonList = []
-                    for js in df_nested_list["vcardArray"]:                     
-                        if js:
-                            jsonList.append(js)  
-                    phone = []
-                
-                    if "registrant" in df_nested_list["roles"]:
-                        print("working")
-                                                
-                    if df_nested_list["roles"][0][0] == "registrant":
-                        for m in jsonList:
-                            
-                            if type(m[1]) == list and any("org" in sl[3] for sl in m[1]):
-                                for l in m[1]:
-                                    if "fn" in l:
-                                        d["org"] = l[3]
-                                    if "tel" in l:
-                                        phone.append(l[3])
-                                        d["phone"] = phone
-                                
-                                    if "email" in l:
-                                        d["email"] = l[3]
-                                        
-                                    if "adr" in l:
-                                        if l[1]:
-                                            d["address"] = l[1]["label"]
-                                        else :
-                                            d["address"]  = ' '.join((str(n) for n in l[3]))
-         
+                    for entity in entities:
+                        for vcard in entity.get("vcardArray", []):
+                            if vcard:
+                                jsonList.append(vcard)
 
-                        if "entities" in df_nested_list:
-                            
-                            indArray = df_nested_list["entities"]
-                            lst = []
-                            
-                            if not isinstance(indArray[0], list):
-                                indArray[0] = []
-                            
-                            
-                            if indArray  is None or  indArray[0] is None : # or not indArray[0]:
-                                print("nothing to do")
-                                            
-                            elif (pd.isnull(indArray[0])).any():
-                                print("nothing to do1")
-                            
-                            else:
-                                
-                                for j in indArray[0]:
-                                    #print(j)
-                                    ind = {}
-                                    for k in j["vcardArray"][1]:
-                                        if "fn" in k:
-                                            ind["name"] = k[3]
-                                            pass
-                                        if "adr" in k:
-                                            if k[1]:
-                                                ind["address"] = k[1]["label"]
-                                            else :
-                                                ind["address"]  = ' '.join((str(n) for n in k[3]))
-        
-                                            pass
-                                        if "email" in k:
-                                            ind["email"] = k[3]
-                                            pass        
-                            
-                                    
-                                    ind["type"] = j["roles"][0]
-                                    lst.append(ind)
-        
-                            d["entities"] = lst
-        
-                    else :
-                        lst = []
-                        tele = []
-                        i = 0
-                        for jData in df_nested_list["vcardArray"]:  
-                            ind = {}
-                            ind["type"] = df_nested_list["roles"][i]
-                            
-                            if "registrant" in df_nested_list["roles"][i]:
-                                if type(jData) == list and any("org" in sl[3] for sl in jData[1]):
-                                    for arr in jData[1]:
-                                        #print(d)                                        
-                                        if "fn" in arr:
-                                            d["org"] = arr[3]                        
-                                        if "adr" in arr:
-                                            d["address"] = arr[1]["label"]
-                                            pass
-                                        if "email" in arr:
-                                            d["email"] = arr[3]
-                                            pass
-                                        if "tel" in arr:
-                                            tele.append(arr[3])
-                                            d["phone"] = tele
-                                            pass 
-                                
-                            i = i+1
-                            tele1 = []
-                            if type(jData) == list:
-                                for arr in jData[1]:
-                                    if "fn" in arr:
-                                        ind["name"] = arr[3]
-                                        pass
-                                    if "adr" in arr:
-                                        ind["address"] = arr[1]["label"]
-                                        pass
-                                    if "email" in arr:
-                                        ind["email"] = arr[3]
-                                        pass
-                                    if "tel" in arr:
-                                        tele1.append(arr[3])
-                                        ind["phone"] = tele1
-                                        pass 
-                            
-                                    
-                                lst.append(ind)
-                            d["entities"] = lst  
+                    phone = []
+                    for m in jsonList:
+                        if isinstance(m[1], list):
+                            for l in m[1]:
+                                if "fn" in l:
+                                    d["org"] = l[3]
+                                if "tel" in l:
+                                    phone.append(l[3])
+                                    d["phone"] = phone
+                                if "email" in l:
+                                    d["email"] = l[3]
+                                if "adr" in l:
+                                    if l[1]:
+                                        d["address"] = l[1].get("label", "")
+                                    else:
+                                        d["address"] = ' '.join((str(n) for n in l[3]))
+
+                    entities_data = []
+                    for entity in entities:
+                        ind = {}
+                        if "vcardArray" in entity:
+                            for vcard in entity["vcardArray"]:
+                                if isinstance(vcard[1], list):
+                                    for item in vcard[1]:
+                                        if "fn" in item:
+                                            ind["name"] = item[3]
+                                        if "adr" in item:
+                                            ind["address"] = item[1].get("label", ' '.join(str(n) for n in item[3]))
+                                        if "email" in item:
+                                            ind["email"] = item[3]
+                                        if "tel" in item:
+                                            ind["phone"] = [item[3]]
+                        if "roles" in entity:
+                            ind["type"] = entity["roles"][0]
+                        if ind:
+                            entities_data.append(ind)
+
+                    d["entities"] = entities_data
+
+                    with open(f'output_json/{filename}', 'w') as outfile:
+                        json.dump(d, outfile, indent=4)
+        except Exception as e:
+            print(f"Error in {filename}")
+            print(e)
+
+# Example usage
+path_of_the_directory = './raw_asn'
+filenames = ['example1.json', 'example2.json']
+writeJson(filenames, path_of_the_directory)
