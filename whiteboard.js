aa6201b468cd59de9a0acacecf212bcf43866a61
fstarsinic <frank.starsinic@gmail.com>
2024-04-09 08:08:46 -0700

    Update whiteboard.js

--- whiteboard.js
+++ whiteboard.js
@@ -1,21 +1,17 @@
-const EventEmitter = require('events').EventEmitter;
-const eventEmitter = new EventEmitter();
+class Mediator {
+  constructor() {
+    this.subscribers = [];
+  }
 
-// Create an instance of the mediator
-const mediator = new Mediator();
+  // Method to add subscribers to the mediator
+  addSubscriber(subscriber) {
+    this.subscribers.push(subscriber);
+  }
 
-// Subscribe mediator to the EventEmitter
-eventEmitter.on('event', (data) => {
-  mediator.notify(data);
-});
-
-// Create instances of subscribers
-const subscriberOne = new SubscriberOne();
-const subscriberTwo = new SubscriberTwo();
-
-// Register subscribers with the mediator
-mediator.addSubscriber(subscriberOne);
-mediator.addSubscriber(subscriberTwo);
-
-// When an event is emitted, all subscribers will be notified through the mediator
-eventEmitter.emit('event', 'This is a test event');
+  // Notify all subscribers about an event
+  notify(eventData) {
+    this.subscribers.forEach(subscriber => {
+      subscriber.update(eventData);
+    });
+  }
+}
